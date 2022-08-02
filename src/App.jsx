d28184f2e9999d19b6b2fa12dd0a3640a3edf0eb
abrusu-collins <snillochemingson@gmail.com>
2022-08-02 22:32:16 +0000

    restructuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,10 @@
-import {BrowserRouter,Switch,Route} from "ReactRouterDOM";
-import "../styles/style.css";
-import NavBar from "./components/navbar";
 
-//Pages
-const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
-};
+import "./styles/style.css";
 
-const About = () => {
-  return (
-    <div>
-      <h1>About</h1>
-    </div>
-  );
-};
-const Blog = () => {
-  return (
-    <div>
-      <h1>Blog</h1>
-    </div>
-  );
-};
-const Contact = () => {
-  return (
-    <div>
-      <h1>Contact Us</h1>
-    </div>
-  );
-};
 
 function App() {
   return (
-    <>
-     
-    <BrowserRouter>
-     <NavBar />
-
-     <div className="pages">
-       <Switch>
-         <Route exact path="/" component={Home} />
-         <Route path="/about" component={About} />
-         <Route path="/blog" component={Blog} />
-         <Route path="/contact" component={Contact} />
-       </Switch>
-     </div>
-   </BrowserRouter>
- </>
-
+    <></>
   );
 }
 
