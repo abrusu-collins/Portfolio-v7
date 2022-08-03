2a92959abda223f77d44bbbeb43c854230ef8deb
abrusu-collins <snillochemingson@gmail.com>
2022-08-03 16:56:18 +0000

    working on projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,33 @@
+import wb from "../img/wb_mockup.png";
 function Projects() {
-    return (
-        <div className="projects">
-            <h1 className="projecthead">Projects</h1>
-            <div className="wordbox"></div>
-            <div className="edemcreatives"></div>
-            <div className="gameisland"></div>
+  return (
+    <div className="projects">
+      <h1 className="projecthead">Projects</h1>
+      <div className="wordbox">
+        <div className="projectwords">
+          <p className="projecttitle">WordBox</p>
+          <p>
+            
+            Built a web app that contains various tools for performing
+            operations on files and words. Some of the tools are Image Text
+            Extractor and PDF reader (PDF to Speech).
+          </p>
+          <p>
+            
+            Developed this web app using HTML, CSS, JavaScript, Bootstrap, Sass,
+            jQuery, express, node, and ejs.
+          </p>
+          <div className="visitproject">
+            <a href="dd">Website</a>
+            <a href="dd">Code</a>
+          </div>
         </div>
-      );
+        <img src={wb} alt="wb_mockup" />
+      </div>
+      <div className="edemcreatives"></div>
+      <div className="gameisland"></div>
+    </div>
+  );
 }
 
 export default Projects;
