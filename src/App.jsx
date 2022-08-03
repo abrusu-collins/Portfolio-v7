13f99ea0c8a78640b2390a0c6af0d749c4e3d7bc
abrusu-collins <snillochemingson@gmail.com>
2022-08-03 19:07:10 +0000

    contact works

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
+import Contacts from "./components/Contacts";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
     <Skills/>
     <Projects/>
     <Experience/>
+    <Contacts/>
     </>
   );
 }
