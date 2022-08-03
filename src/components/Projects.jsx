4b926dabedb693e07beeb94f8fc4c8f33461790d
abrusu-collins <snillochemingson@gmail.com>
2022-08-03 20:26:09 +0000

    wb links updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,21 +9,21 @@ function Projects() {
         <div className="projectwords">
           <p className="projecttitle">WordBox</p>
           <div className="description">
-          <p>
-            
-            Built a web app that contains various tools for performing
-            operations on files and words. Some of the tools are Image Text
-            Extractor and PDF reader (PDF to Speech).
-          </p>
-          <p>
-            
-            Developed this web app using HTML5, CSS3, JavaScript, Bootstrap, Sass,
-            jQuery, express, node, and ejs.
-          </p>
+            <p>
+              Built a web app that contains various tools for performing
+              operations on files and words. Some of the tools are Image Text
+              Extractor and PDF reader (PDF to Speech).
+            </p>
+            <p>
+              Developed this web app using HTML5, CSS3, JavaScript, Bootstrap,
+              Sass, jQuery, express, node, and ejs.
+            </p>
           </div>
           <div className="visitproject">
-            <a href="dd" id="website">Website</a>
-            <a href="dd">Code</a>
+            <a href="https://clisteocrr.herokuapp.com/" id="website">
+              Website
+            </a>
+            <a href="https://github.com/abrusu-collins/WordBox">Code</a>
           </div>
         </div>
         <img src={wb} alt="wb_mockup" />
@@ -33,18 +33,18 @@ function Projects() {
         <div className="projectwords">
           <p className="projecttitle">Edem Creatives</p>
           <div className="description">
-          <p>
-            
-          Designed and developed a site for a graphic designer, this helped people find his works and order designs from him 
-through the website. After launching the website, he went from getting fewer than 10 orders a week to 25+ orders a 
-week.
-          </p>
-          <p>
-          Developed this site using HTML5, CSS3, and JavaScript.
-          </p>
+            <p>
+              Designed and developed a site for a graphic designer, this helped
+              people find his works and order designs from him through the
+              website. After launching the website, he went from getting fewer
+              than 10 orders a week to 25+ orders a week.
+            </p>
+            <p>Developed this site using HTML5, CSS3, and JavaScript.</p>
           </div>
           <div className="visitproject">
-            <a href="dd" id="website">Website</a>
+            <a href="dd" id="website">
+              Website
+            </a>
             <a href="dd">Code</a>
           </div>
         </div>
@@ -55,31 +55,26 @@ week.
         <div className="projectwords">
           <p className="projecttitle">Game Island</p>
           <div className="description">
-          <p>
-            
-          Developed a website that contains a collection of memory games.
-
-          </p>
-          <p>
-          Some of the games are Fast Fingers (a typing game that helps users increase their typing speed), Verbal Memory (a 
-game that tests how many words you can keep in your short-term memory), and Reaction Time (a game that measures 
-how fast you can react to change)
-
-          </p>
-          <p>
-            
-          Built it using HTML5, CSS3, Sass, Bootstrap, and JavaScript.
-
-          </p>
+            <p>
+              Developed a website that contains a collection of memory games.
+            </p>
+            <p>
+              Some of the games are Fast Fingers (a typing game that helps users
+              increase their typing speed), Verbal Memory (a game that tests how
+              many words you can keep in your short-term memory), and Reaction
+              Time (a game that measures how fast you can react to change)
+            </p>
+            <p>Built it using HTML5, CSS3, Sass, Bootstrap, and JavaScript.</p>
           </div>
           <div className="visitproject">
-            <a href="dd" id="website">Website</a>
+            <a href="dd" id="website">
+              Website
+            </a>
             <a href="dd">Code</a>
           </div>
         </div>
         <img src={gi} alt="gi_mockup" />
       </div>
-      
     </div>
   );
 }
