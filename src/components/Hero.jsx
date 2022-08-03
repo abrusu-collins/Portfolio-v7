8b64d3fab3fab418664467f6fcd3aa3d8bdd1b09
abrusu-collins <snillochemingson@gmail.com>
2022-08-03 10:59:22 +0000

    added an avatar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,20 @@
+import avatar from "../img/avatar.webp"
 function Hero() {
     return ( 
         <div className="hero">
+            <div className="leftside">
             <h1 id="name">Collins Abrusu</h1>
             <h1 id="job">Front End Engineer</h1>
             <div id="links">
-                <a href="">GitHub</a>
-                <a href="">Twitter</a>
-                <a href="">LinkedIn</a>
-                <a href="">Gmail</a>
-                <a href="">Resume</a>
+                <a href="dd">GitHub</a>
+                <a href="dd">Twitter</a>
+                <a href="dd">LinkedIn</a>
+                <a href="dd">Gmail</a>
+                <a href="dd">Resume</a>
             </div>
+            </div>
+           <img src={avatar} alt="avatar" />
+
         </div>
      );
 }
