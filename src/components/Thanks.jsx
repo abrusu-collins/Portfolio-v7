ee4de5543c04bb5c9b15498715d498f64c1aa119
abrusu-collins <snillochemingson@gmail.com>
2022-08-03 20:09:17 +0000

    working on thank you

---
+++ src/components/Thanks.jsx
@@ -0,0 +1,9 @@
+function Thanks() {
+    return ( 
+        <div className="thanks">
+            <h1 className="thankhead">Thanks for visiting</h1>
+        </div>
+     );
+}
+
+export default Thanks;
