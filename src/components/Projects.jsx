46cf70b8c08e685a4b856ae6daf55d351843140b
abrusu-collins <snillochemingson@gmail.com>
2022-08-03 18:02:34 +0000

    adding mockups

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import wb from "../img/wb_mockup.png";
+import ec from "../img/ec_mockup.png";
 function Projects() {
   return (
     <div className="projects">
@@ -29,18 +30,16 @@ function Projects() {
 
       <div className="project">
         <div className="projectwords">
-          <p className="projecttitle">WordBox</p>
+          <p className="projecttitle">Edem Creatives</p>
           <div className="description">
           <p>
             
-            Built a web app that contains various tools for performing
-            operations on files and words. Some of the tools are Image Text
-            Extractor and PDF reader (PDF to Speech).
+          Designed and developed a site for a graphic designer, this helped people find his works and order designs from him 
+through the website. After launching the website, he went from getting fewer than 10 orders a week to 25+ orders a 
+week.
           </p>
           <p>
-            
-            Developed this web app using HTML, CSS, JavaScript, Bootstrap, Sass,
-            jQuery, express, node, and ejs.
+          Developed this site using HTML5, CSS3, and JavaScript.
           </p>
           </div>
           <div className="visitproject">
@@ -48,7 +47,7 @@ function Projects() {
             <a href="dd">Code</a>
           </div>
         </div>
-        <img src={wb} alt="wb_mockup" />
+        <img src={ec} alt="wb_mockup" />
       </div>
 
       <div className="project">
