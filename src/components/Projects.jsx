3d7ac489f8b5fdf86e2be38c953a11c944cbca51
abrusu-collins <snillochemingson@gmail.com>
2022-11-15 18:34:13 +0000

    cool

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
-import img1 from "../img/mawuli.png";
+import img1 from "../img/antonio.png";
 import img2 from "../img/mawuli2.png";
+import img3 from "../img/hotelvivi.png";
 
 function Projects() {
   return (
@@ -7,7 +8,7 @@ function Projects() {
       <p className="projects-title">Recent Works✨</p>
       <div className="project1" id="projects">
         <div className="project-about">
-          <p className="project-name">Mawuli's Art</p>
+          <p className="project-name">Mawuli's Art🎨</p>
           <p className="description">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero esse
             sapiente facere repudiandae inventore. Aut mollitia ipsum, eius modi
@@ -20,6 +21,38 @@ function Projects() {
           </div>
         </div>
         <img src={img2} alt="" />
+      </div>
+      <div className="project2" id="projects">
+      <img src={img1} alt="" />
+        <div className="project-about">
+          <p className="project-name">Antonio Fotografo📸</p>
+          <p className="description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero esse
+            sapiente facere repudiandae inventore. Aut mollitia ipsum, eius modi
+            hic beatae maiores ad sequi dolor debitis, ipsam odit asperiores
+            temporibus?
+          </p>
+          <div className="project-links">
+            <a href="">View Code</a>
+            <a href="">Live Link</a>
+          </div>
+        </div>
+      </div>
+      <div className="project3" id="projects">
+        <div className="project-about">
+          <p className="project-name">Hotelvivi🏨</p>
+          <p className="description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero esse
+            sapiente facere repudiandae inventore. Aut mollitia ipsum, eius modi
+            hic beatae maiores ad sequi dolor debitis, ipsam odit asperiores
+            temporibus?
+          </p>
+          <div className="project-links">
+            <a href="">View Code</a>
+            <a href="">Live Link</a>
+          </div>
+        </div>
+        <img src={img3} alt="" />
       </div>
     </div>
   );
