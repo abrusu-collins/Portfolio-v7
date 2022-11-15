b5bc9d2757134754231aa4553314b0f1d039e8ef
abrusu-collins <snillochemingson@gmail.com>
2022-11-15 17:27:58 +0000

    cool

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./styles/index.css";
 import Hero from "./components/Hero";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 function App() {
   return (
     <>
     <Hero/>
     <Skills/>
+    <Projects/>
     </>
   );
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,12 @@
+function Projects() {
+    return ( 
+        <div className="projects">
+            <p className="projects-title">Recent Works✨</p>
+            <div className="project1" id="projects">
+
+            </div>
+        </div>
+     );
+}
+
+export default Projects;
