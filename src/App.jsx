469b65ab3594f6771e685c581ed382747d22ebb5
abrusu-collins <snillochemingson@gmail.com>
2022-11-15 19:00:41 +0000

    contacts

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./styles/index.css";
 import Hero from "./components/Hero";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import Contacts from "./components/Contacts";
 function App() {
   return (
     <>
     <Hero/>
     <Skills/>
     <Projects/>
+    <Contacts/>
     </>
   );
 }
---
+++ src/components/Contacts.jsx
@@ -0,0 +1,12 @@
+function Contacts() {
+    return ( 
+        <div className="contacts">
+            <p className="touch">Get in touch🤙</p>
+            <div className="socials">
+                
+            </div>
+        </div>
+     );
+}
+
+export default Contacts;
