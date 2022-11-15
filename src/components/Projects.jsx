7e8126fdc2602a6b43824f3ce8a85f84f08f1ccd
abrusu-collins <snillochemingson@gmail.com>
2022-11-15 18:37:10 +0000

    cool

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import img1 from "../img/antonio.png";
 import img2 from "../img/mawuli2.png";
 import img3 from "../img/hotelvivi.png";
+import img4 from "../img/devtech.png";
 
 function Projects() {
   return (
@@ -53,6 +54,22 @@ function Projects() {
           </div>
         </div>
         <img src={img3} alt="" />
+      </div>
+      <div className="project4" id="projects">
+      <img src={img4} alt="" />
+        <div className="project-about">
+          <p className="project-name">DevTech💻</p>
+          <p className="description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero esse
+            sapiente facere repudiandae inventore. Aut mollitia ipsum, eius modi
+            hic beatae maiores ad sequi dolor debitis, ipsam odit asperiores
+            temporibus?
+          </p>
+          <div className="project-links">
+            <a href="">View Code</a>
+            <a href="">Live Link</a>
+          </div>
+        </div>
       </div>
     </div>
   );
