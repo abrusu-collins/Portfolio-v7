970c9a83cbdde49aba3ae418934b6d64a500bb0e
abrusu-collins <snillochemingson@gmail.com>
2022-11-15 16:02:29 +0000

    cool

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./styles/index.css";
 import Hero from "./components/Hero";
-
+import Skills from "./components/Skills";
 function App() {
   return (
     <>
     <Hero/>
+    <Skills/>
     </>
   );
 }
---
+++ src/components/Skills.jsx
@@ -0,0 +1,57 @@
+function Skills() {
+    return ( 
+        <div className="skills">
+            <p className="skills-header">What I know🛠️</p>
+            <div className="skills-innner">
+                <div className="langs">
+                    <p className="title">Languages, Frameworks and Libraries</p>
+                    <div className="the-skills">
+                        <p>HTML5</p>
+                        <p>CSS3</p>
+                        <p>JavaScript</p>
+                        <p>Bootstrap</p>
+                        <p>Sass</p>
+                        <p>CSS Modules</p>
+                        <p>React</p>
+                        <p>Redux</p>
+                        <p>Node</p>
+                        <p> Jest</p>
+                        <p> REST APIs</p>
+                    </div>
+                </div>
+                <div className="tools">
+                <p className="title">Tools</p>
+                    <div className="the-skills">
+                        <p>Git</p>
+                        <p>VSCode</p>
+                        <p>NPM</p>
+                        <p>Yarn</p>
+                        <p>Webpack</p>
+                    </div>
+                </div>
+                <div className="platforms">
+                <p className="title">Platforms</p>
+                    <div className="the-skills">
+                        <p>GitHub</p>
+                        <p>Heroku</p>
+                        <p>Netlify</p>
+                        <p>Vercel</p>
+                    </div>
+                </div>
+                <div className="soft">
+                <p className="title">Soft Skills</p>
+                    <div className="the-skills">
+                        <p> Leadership</p>
+                        <p>Writing</p>
+                        <p> Public Speaking</p>
+                        <p>Time Management</p>
+                        <p>Team Player</p>
+                        <p>Fast Learner</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+     );
+}
+
+export default Skills;
