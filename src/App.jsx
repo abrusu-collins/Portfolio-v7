2458ee4953afe761c3563adc31e501cd4a38f7a7
abrusu-collins <snillochemingson@gmail.com>
2022-11-15 12:56:53 +0000

    cool

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
 import "./styles/style.css";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Experience from "./components/Experience";
-import Contacts from "./components/Contacts";
-import Thanks from "./components/Thanks";
 
 function App() {
   return (
     <>
-    <Hero/>
-    <About/>
-    <Skills/>
-    <Projects/>
-    <Experience/>
-    <Contacts/>
-    <Thanks/>
     </>
   );
 }
