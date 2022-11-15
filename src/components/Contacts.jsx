e92d4a22d63ad8e9e9c2bf1e47691d120185b47d
abrusu-collins <snillochemingson@gmail.com>
2022-11-15 19:15:12 +0000

    cool

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,9 +1,15 @@
+import twitter from "../img/twitter.png";
+import linkedin from "../img/linkedin.png";
+import github from "../img/github.png";
+
 function Contacts() {
     return ( 
         <div className="contacts">
             <p className="touch">Get in touch🤙</p>
             <div className="socials">
-                
+            <a href=""><img src={twitter} alt="" /></a>
+            <a href=""><img src={linkedin} alt="" /></a>
+            <a href=""><img src={github} alt="" /></a>
             </div>
         </div>
      );
