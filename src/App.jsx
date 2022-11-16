6923c8690839354e3861b360492714eb254c683a
abrusu-collins <snillochemingson@gmail.com>
2022-11-16 10:56:25 +0000

    nice

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
+import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
 import "./styles/index.css";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Contacts from "./components/Contacts";
+import Home from "./components/Home";
+
+
 function App() {
   return (
     <>
+    <Router>
     <Navbar/>
-    <Hero/>
-    <Projects/>
-    <Skills/>
-    <Contacts/>
+    <Routes >
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+  </Router>
     </>
   );
 }
