0bdfc10a1c891f76368b22017eadf370cacf68f3
abrusu-collins <snillochemingson@gmail.com>
2023-07-02 18:35:11 +0000

    json files done

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Testimonials() {
+  return (
+    <div className='testimonials'>
+        <p className="title">Don't just take my word for it</p>
+    </div>
+  )
+}
+
+export default Testimonials
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
+import Testimonials from "@/components/Testimonials";
 export default function Home() {
   return (
     <>
       <Hero />
       <Projects/>
       <Skills/>
+      <Testimonials/>
     </>
   );
 }
