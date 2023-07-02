b6f7018e135ce1556d7ed43c4d5c5a7c305a1e1a
abrusu-collins <snillochemingson@gmail.com>
2023-07-02 21:09:30 +0000

    cool one

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { BsTwitter, BsSlack } from "react-icons/bs";
 import left_testimonial_content from "../json_files/left_testimonials.json";
+import right_testimonial_content from "../json_files/right_testimonials.json";
+
 function Testimonials() {
   return (
     <div className="testimonials">
-      <p className="title">Don't just take my word for it</p>
+      {/* <p className="title">Don't just take my word for it</p> */}
+
+
+
+
 
       <div className="left-infinite">
         {left_testimonial_content.map((left_testimonial_item) => {
@@ -19,6 +25,25 @@ function Testimonials() {
                 )}
               </div>
               <p className="testimony">{left_testimonial_item.testimony}</p>
+            </div>
+          );
+        })}
+      </div>
+
+
+      <div className="right-infinite">
+        {right_testimonial_content.map((right_testimonial_item) => {
+          return (
+            <div key={right_testimonial_item.name} >
+              <div className="testifier">
+                <p>{right_testimonial_item.testifier}</p>
+                {right_testimonial_item.platform == "slack" ? (
+                  <BsSlack />
+                ) : (
+                  <BsTwitter />
+                )}
+              </div>
+              <p className="testimony">{right_testimonial_item.testimony}</p>
             </div>
           );
         })}
