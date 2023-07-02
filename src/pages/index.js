5cf64269b9c382d9bc3a9eed8e7adbd8785bcf25
abrusu-collins <snillochemingson@gmail.com>
2023-07-02 21:39:56 +0000

    cool

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import Testimonials from "@/components/Testimonials";
 import TestimonialHeader from "@/components/TestimonialHeader";
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <>
@@ -11,6 +12,7 @@ export default function Home() {
       <Skills/>
       <TestimonialHeader/>
       <Testimonials/>
+      <Footer/>
     </>
   );
 }
