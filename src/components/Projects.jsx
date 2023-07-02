8add5f938dfe0ffa55a2021900d8a54e0fd19f9c
abrusu-collins <snillochemingson@gmail.com>
2023-07-02 16:14:40 +0000

    neat

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,12 +10,25 @@ import { SiHotelsdotcom } from "react-icons/si";
 import { RiPaintFill } from "react-icons/ri";
 import projectContent from "../json_files/projects.json";
 function Projects() {
-  const r = [
+  const allLogo = [
     <MdHideImage key={0} />,
     <MdDriveFileRenameOutline key={1} />,
     <BiSolidBible key={2} />,
     <IoLogoGameControllerB key={3} />,
     <TbTopologyComplex key={4} />,
+    <SiHotelsdotcom key={5} />,
+    <BiCodeAlt key={6} />,
+    <RiPaintFill key={7} />,
+    <BsCameraFill key={8} />,
+  ];
+  const appsLogo = [
+    <MdHideImage key={0} />,
+    <MdDriveFileRenameOutline key={1} />,
+    <BiSolidBible key={2} />,
+    <IoLogoGameControllerB key={3} />,
+    <TbTopologyComplex key={4} />,
+  ];
+  const sitesLogo = [
     <SiHotelsdotcom key={5} />,
     <BiCodeAlt key={6} />,
     <RiPaintFill key={7} />,
@@ -55,38 +68,79 @@ function Projects() {
         </a>
         <a
           href=""
-          className={appsToggle ? "active-toggle" : ""}
-          onClick={toggleApps}
-        >
-          Web Apps
-        </a>
-        <a
-          href=""
           className={sitesToggle ? "active-toggle" : ""}
           onClick={toggleSites}
         >
           Websites
         </a>
+        <a
+          href=""
+          className={appsToggle ? "active-toggle" : ""}
+          onClick={toggleApps}
+        >
+          Web Apps
+        </a>
       </div>
 
       <div className="projecct-cards">
-        {projectContent.map((projectItem) => {
-          return (
-            <div className="all" key={projectItem.name}>
-              <div className="logo"> {r[projectItem.id]}</div>
-              <p className="project-name">{projectItem.name}</p>
-              <p className="description">{projectItem.description}</p>
-              <div className="links">
-                <a href={projectItem.github}>
-                  <BsGithub />
-                </a>
-                <a href={projectItem.live}>
-                  <BsGlobe />
-                </a>
+        {allToggle &&
+          projectContent.map((projectItem) => {
+            return (
+              <div className="all" key={projectItem.name}>
+                <div className="logo"> {allLogo[projectItem.id]}</div>
+                <p className="project-name">{projectItem.name}</p>
+                <p className="description">{projectItem.description}</p>
+                <div className="links">
+                  <a href={projectItem.github} target="_blank">
+                    <BsGithub />
+                  </a>
+                  <a href={projectItem.live} target="_blank">
+                    <BsGlobe />
+                  </a>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        {appsToggle &&
+          projectContent.map((projectItem) => {
+            return (
+              projectItem.type == "webapp" && (
+                <div className="all" key={projectItem.name}>
+                  <div className="logo"> {appsLogo[projectItem.id]}</div>
+                  <p className="project-name">{projectItem.name}</p>
+                  <p className="description">{projectItem.description}</p>
+                  <div className="links">
+                    <a href={projectItem.github} target="_blank">
+                      <BsGithub />
+                    </a>
+                    <a href={projectItem.live} target="_blank">
+                      <BsGlobe />
+                    </a>
+                  </div>
+                </div>
+              )
+            );
+          })}
+        {sitesToggle &&
+          projectContent.map((projectItem) => {
+            return (
+              projectItem.type == "website" && (
+                <div className="all" key={projectItem.name}>
+                  <div className="logo"> {sitesLogo[projectItem.id - 5]}</div>
+                  <p className="project-name">{projectItem.name}</p>
+                  <p className="description">{projectItem.description}</p>
+                  <div className="links">
+                    <a href={projectItem.github} target="_blank">
+                      <BsGithub />
+                    </a>
+                    <a href={projectItem.live} target="_blank">
+                      <BsGlobe />
+                    </a>
+                  </div>
+                </div>
+              )
+            );
+          })}
       </div>
     </div>
   );
