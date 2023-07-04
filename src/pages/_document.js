800106d3e733ec7dcec041d48c773afa9dd907b7
abrusu-collins <snillochemingson@gmail.com>
2023-07-04 12:13:18 +0000

    favicon added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,26 +4,25 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <Head>
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/favicon_io/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon_io/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon_io/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/favicon_io/site.webmanifest" />
-        </Head>
+        <title> Collins | Portfolio</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <body>
         <Main />
