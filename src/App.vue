c1fc8457a78e753ff68ed22c39897514a13bf8e9
abrusu-collins <snillochemingson@gmail.com>
2024-08-20 00:45:19 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+export default {
+  name: " App"
+}
+</script>
+
+<template>
+  <div>
+    <p>
+      Hi
+    </p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+div p {
+  color: red;
+}
+</style>
