0f078ebc8c8f4944c6d1f39297597f42ffbc2d76
abrusu-collins <snillochemingson@gmail.com>
2024-08-21 23:20:52 +0000

    time working like magic🕝

--- src/components/General/Navbar.vue
+++ src/components/General/Navbar.vue
@@ -1,27 +1,47 @@
 <template>
     <nav>
-       <div class="nav_inner">
-        <p class="initla">ca<span>.</span> </p>
+        <div class="nav_inner">
+            <p class="initla">ca<span>.</span> </p>
 
-<div class="gh_time">
-    <!-- <img src="../../../public/images/gh.svg" alt=""> -->
-    <!-- <img src="../../../public/images/GHA.svg" alt=""> -->
-    <img src="../../../public/images/gha.webp" alt="">
-    <p class="time">2:20PM</p>
+            <div class="gh_time">
+                <img src="../../../public/images/gha.webp" alt="">
+                <p class="time">{{ time }} </p>
 
-</div>
+            </div>
 
-<a href="" class="open_menu"><IconHamburger/></a>
-       </div>
+            <a href="" class="open_menu">
+                <IconHamburger />
+            </a>
+        </div>
     </nav>
 </template>
 
 <script lang="ts">
+import moment from 'moment';
 import IconHamburger from '../icons/IconHamburger.vue';
-
+interface Data {
+    time: string | null,
+}
 export default {
     name: "Navbar",
-    components:{IconHamburger}
+    components: { IconHamburger },
+    data(): Data {
+        return {
+            time: null,
+        }
+    },
+    mounted() {
+        this.getTime();
+    },
+    methods: {
+        getTime() {
+
+            setInterval(() => {
+                this.time = moment(new Date()).format("HH:mm:ss a");
+            }, 1000);
+        }
+        ,
+    }
 }
 </script>
 
@@ -34,32 +54,35 @@ nav {
     top: 0;
     z-index: 100;
     border-bottom: 0.4px solid $grey_line;
- & .nav_inner{
-    width: 80%;
-    margin: auto;
-    font-size: 20px;
-    @include flex_base(center, space-between, null, null);
-    & .gh_time{
-        @include flex_base(center, center, 10px, null);
-        /* display: inline-grid; */
-        /* background-color: red; */
-        /* overflow: hidden; */
 
-        & img{
-            width: 80px;
-        }
-        & .time{
+    & .nav_inner {
+        width: 80%;
+        margin: auto;
+        font-size: 20px;
+        @include flex_base(center, space-between, null, null);
+
+        & .gh_time {
+            @include flex_base(center, center, 10px, null);
+            /* display: inline-grid; */
             /* background-color: red; */
+            /* overflow: hidden; */
+
+            & img {
+                width: 80px;
+            }
+
+            & .time {
+                /* background-color: red; */
+            }
+        }
+
+        & .open_menu {
+            background-color: $white;
+            border-radius: 50%;
+            height: 50px;
+            width: 50px;
+            @include flex_base(center, center, null, null);
         }
     }
-
-    & .open_menu{
-        background-color: $white;
-        border-radius: 50%;
-        height: 50px;
-        width: 50px;
-        @include flex_base(center, center, null,null);
-    }
- }
 }
 </style>
